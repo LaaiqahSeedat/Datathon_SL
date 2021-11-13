7e595d4f56b92f003d95c602204cc35988742ecc
Omega-Andries <andrieschimule@gmail.com>
2021-11-13 14:34:29 +0200

    added the datasets

--- Backend/MentalApp/views.py
+++ Backend/MentalApp/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
-
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
 # Create your views here.
+
+def home(request):
+    api_urls = {
+        'Test URL':'test/',
+    }
+    return Response(api_urls)
+
+def test(request):
+    respose_json = {
+        'Test':'I got you'
+    }
+    return Response(respose_json)
