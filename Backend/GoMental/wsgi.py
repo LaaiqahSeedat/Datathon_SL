86161f8ed6414003ac9cd48a744d68d8c8fb6201
Omega-Andries <andrieschimule@gmail.com>
2021-11-13 11:16:03 +0200

    added new backend service to fit the theme

--- Backend/GoMental/wsgi.py
+++ Backend/GoMental/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for BostonProject project.
+WSGI config for GoMental project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'BostonProject.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'GoMental.settings')
 
 application = get_wsgi_application()
