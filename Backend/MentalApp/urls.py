4c760aa3c3d116be99811111b8c1ff9bf1b9ae1a
Omega-Andries <andrieschimule@gmail.com>
2021-11-13 16:53:49 +0200

    added angular template

--- Backend/MentalApp/urls.py
+++ Backend/MentalApp/urls.py
@@ -1,1 +1,8 @@
+from os import name
+from django.urls import path as p
+from . import views as v 
 
+urlpatterns = [
+    p('', v.home,name="Home"),
+    p('general_mental_health_check/', v.gmentalh, name="General Mental Health"),
+]
--- Backend/MentalApp/views.py
+++ Backend/MentalApp/views.py
@@ -3,14 +3,20 @@ from rest_framework.decorators import api_view
 from rest_framework.response import Response
 # Create your views here.
 
+@api_view(['GET'])
 def home(request):
     api_urls = {
-        'Test URL':'test/',
+        'General Mental Health URL':'general_mental_health_check/',
     }
     return Response(api_urls)
 
-def test(request):
+@api_view(['POST'])
+def gmentalh(request):
+    theData = request.data
+    
+    print(theData.get("Gender"))
+
     respose_json = {
-        'Test':'I got you'
+        'Result':'You are at risk of ADHD'
     }
     return Response(respose_json)
